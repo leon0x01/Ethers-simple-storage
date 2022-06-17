0e75d0551ca360c338e74bf0ee635efbc94a4621
leon0X01 <ushrestha358@gmail.com>
2022-06-17 17:32:24 +0545

    encrypted the private key

--- deploy.js
+++ deploy.js
@@ -1,15 +1,15 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 
 async function main(){
     //compile them in our code
     //compile them separately
-    // HTTP://0.0.0.0:7545
-    const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:7545"); // connection to blockchain
-    const wallet = new ethers.Wallet(
-        "bdc18aa140576d28a075f48a82ee1288344156891f73dfdcdf939439f0812f1d",
-        provider
-    ); // creation of wallet need the private key and provider 
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL); // connection to blockchain
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+     // creation of wallet need the private key and provider 
+    let wallet =  new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    wallet = await wallet.connect(provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
